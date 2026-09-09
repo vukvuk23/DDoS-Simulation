f8be8fc863f5dcaf24cbce395eb3dc080e551a8f
Vuk Tajsic <vuk.tajsic@gmail.com>
2026-09-09 20:32:06 +0200

    modify legit_client traffick to average 10 req/s

--- legit_client/client.py
+++ legit_client/client.py
@@ -1,5 +1,7 @@
+import os
 import time
 import random
+import threading
 import requests
 
 TARGET_HOST = "target.local"          
@@ -9,12 +11,14 @@ UPLOAD_ROUTE = "/api/upload" # izdvojena, zahteva telo
 
 UPLOAD_PROBABILITY = 0.1
 
+NUM_VIRTUAL_USERS = int(os.environ.get("NUM_VIRTUAL_USERS", 50)) 
 
-def main():
+
+def simulate_user():
 
     while True: 
 
-        wait_time = random.expovariate(1 / 30) # 30 je prosek kroz mnogo poziva, wait time ce biti br koji prati eksp rasp
+        wait_time = random.expovariate(1 / 8) # 8 je prosek kroz mnogo poziva, wait time ce biti br koji prati eksp rasp
         time.sleep(wait_time) # zavrsava izvrsavanje na zadat br s
 
         number_of_clicks = random.randint(1, 4) 
@@ -22,7 +26,7 @@ def main():
         for _ in range(number_of_clicks):
 
             if random.random() < UPLOAD_PROBABILITY:
-               # vraca rand iz 0.0 - 1.0
+                # vraca rand iz 0.0 - 1.0
                 try:
                     requests.post(
                         "http://host.docker.internal" + UPLOAD_ROUTE,
@@ -33,7 +37,6 @@ def main():
                     pass 
 
             else:
-                
                 route = random.choice(LEGIT_ROUTES)
 
                 try:
@@ -46,6 +49,17 @@ def main():
                     pass  
 
             time.sleep(random.uniform(0.5, 3))   
+
+
+def main(): 
+
+    for _ in range(NUM_VIRTUAL_USERS): 
+        t = threading.Thread(target=simulate_user, daemon=True) 
+        t.start() # pokreni odmah
+
+    while True: # glavna nit samo spava zauvek dok pozadinske niti rade posao
+        time.sleep(60)
+
 
 if __name__ == "__main__":
     main()
